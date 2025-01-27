ef2ea2461be38f81eb3347c9c8a2e5395730f77c
tapha10 <99338722+tapha10@users.noreply.github.com>
2025-01-27 15:14:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import pickle
 import zipfile
 
 # Configuration de la page
